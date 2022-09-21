7c034a88bc5f40af142298a0087e986d7104e7df
Rashid Mingazov <rashid.mingazov@mircod.com>
2022-09-21 19:19:58 +0300

    !changed user id

--- src/main.py
+++ src/main.py
@@ -36,16 +36,19 @@ async def on_ready():
 class MessageHandlers:
     @staticmethod
     async def soyjack_reply(message):
-        lucky_number = random.randint(0, 20)
-
         # if garv
-        if message.author.id in (949379472912707639,):
-            if 5 < lucky_number < 20:
-                logger.info(f"{message.author.name} ROLLED PIC!")
-                await message.channel.send("https://media.discordapp.net/attachments/913204760650334259/928569786156335124/crazy_cat_bubble.gif")
-                return True
+        if message.author.id == 949379472912707639:
+            lucky = random.randint(1, 3) == 2
+        else:
+            lucky = random.randint(0, 20) == 1
 
-        if lucky_number == 2:
+        # if message.author.id in (301676192900317184, 315029938950635520):
+        #     if 5 < lucky_number < 20:
+        #         logger.info(f"{message.author.name} ROLLED PIC!")
+        #         await message.channel.send("https://media.discordapp.net/attachments/913204760650334259/928569786156335124/crazy_cat_bubble.gif")
+        #         return True
+
+        if lucky:
             logger.info(f"{message.author.name} ROLLED PIC!")
             await message.channel.send(f">{message.content}", file=discord.File(random.choice(get_pics())))
             return True
